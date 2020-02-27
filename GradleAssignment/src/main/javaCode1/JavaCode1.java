f6c49fe1631acbd69c415c400bb672f9d60de3a5
aman-ttn <aman.saini@tothenew.com>
2020-02-27 11:11:46 +0530

    Gradle Assignment

---
+++ GradleAssignment/src/main/javaCode1/JavaCode1.java
@@ -0,0 +1,7 @@
+package main.javaCode1;
+
+public class JavaCode1 {
+    public static void main(String[] args) {
+        System.out.println("Hello JavaCode1");
+    }
+}
---
+++ GradleAssignment/src/main/javaCode2/JavaCode2.java
@@ -0,0 +1,7 @@
+package main.javaCode2;
+
+public class JavaCode2 {
+    public static void main(String[] args) {
+        System.out.println("Hello JavaCode2");
+    }
+}
