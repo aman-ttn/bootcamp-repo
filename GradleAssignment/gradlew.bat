b79bcb82a6f8a36193a848e2ef8458c08b6e149c
aman-ttn <aman.saini@tothenew.com>
2020-02-27 12:37:20 +0530

    Gradle Assignment

--- GradleAssignment/gradlew.bat
+++ GradleAssignment/gradlew.bat
@@ -15,7 +15,9 @@ set APP_HOME=%DIRNAME%
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS=
-
+include 'Question2'
+include 'Question3'
+include 'Question4'
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
 
