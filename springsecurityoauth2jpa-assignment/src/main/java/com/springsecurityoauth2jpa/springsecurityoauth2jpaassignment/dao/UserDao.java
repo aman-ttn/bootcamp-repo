c5e59b737763ffb0dbec56731ed33754b3a1d111
aman-ttn <aman.saini@tothenew.com>
2020-03-24 07:00:29 +0530

    Implementing Security with assigning different role to single user and did the password implementation

--- springsecurityoauth2jpa-assignment/src/main/java/com/springsecurityoauth2jpa/springsecurityoauth2jpaassignment/dao/UserDao.java
+++ springsecurityoauth2jpa-assignment/src/main/java/com/springsecurityoauth2jpa/springsecurityoauth2jpaassignment/dao/UserDao.java
@@ -19,7 +19,8 @@ public class UserDao {
         User user = userRepository.findByUsername(username);
         System.out.println(user);
         if (username != null) {
-            return new AppUser(user.getUsername(), user.getPassword(), Arrays.asList(new GrantAuthorityImpl(user.getRole())));
+            return new AppUser(user.getUsername(), user.getPassword(),
+                    Arrays.asList(new GrantAuthorityImpl(user.getRole())));
         } else {
             throw new RuntimeException();
         }
